282a6f99a27e31979e26caf10f89099a032bb741
KaviyaPriya <kavyapriya2702@gmail.com>
2024-01-24 18:47:00 +0530

    first commit

---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//Functional component:
+function FunctionalComponent()  {
+  return (                                                                                                                                                                                                                     
+    <div>
+        <h1>This is my functional Component</h1>
+    </div>
+  );
+}
+
+export default FunctionalComponent;
